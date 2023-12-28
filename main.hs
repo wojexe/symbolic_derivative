2498a49f551773848df68a42e7efa2c6b761c126
Wojciech Sierotowicz <wojexe@gmail.com>
2023-12-29 00:52:46 +0100

    Parsing initially done

--- main.hs
+++ main.hs
@@ -1,3 +1,37 @@
+import Control.Monad
+import Data.Functor
+import Text.Parsec
+import Text.Parsec.String
+
+main :: IO ()
+main = do
+  putStrLn "Enter math expression to be derived (possible variables: x, y, z):"
+  input <- getLine
+
+  when (hasUnmatchedParens input) $ error "The math expression you provided has unmatched parentheses"
+
+  case parseExpression (squeeze input) of
+    Left err -> putStrLn $ "Error while parsing: " ++ show err
+    Right expr -> do
+      putStrLn $ "Parsed expression: " ++ show expr
+      putStrLn "Partial derivatives:"
+      putStrLn $ "  d/dx: " ++ show (dx expr)
+      putStrLn $ "  d/dy: " ++ show (dy expr)
+      putStrLn $ "  d/dz: " ++ show (dz expr)
+
+squeeze :: String -> String
+squeeze = filter (/= ' ')
+
+hasUnmatchedParens :: String -> Bool
+hasUnmatchedParens str = left /= right
+  where
+    left = length $ filter (== '(') str
+    right = length $ filter (== ')') str
+
+--
+-- IMPLEMENTATION
+--
+
 data Expr a
   = Var Char
   | Const a
@@ -17,11 +51,11 @@ instance (Show a) => Show (Expr a) where
   showsPrec prec (Var ch) = showChar ch
   showsPrec prec (Const a) = showsPrec prec a
   showsPrec prec (Neg a) = showParen True $ showChar '-' . showsPrec 9 a
-  showsPrec prec (Add a (Neg b)) = showParen (prec >= 7) $ showsPrec 7 a . showString " - " . showsPrec 7 b
-  showsPrec prec (Add a b) = showParen (prec >= 7) $ showsPrec 7 a . showString " + " . showsPrec 7 b
-  showsPrec prec (Mul a b) = showParen (prec >= 8) $ showsPrec 8 a . showString " * " . showsPrec 8 b
-  showsPrec prec (Div a b) = showParen (prec >= 8) $ showsPrec 8 a . showString " / " . showsPrec 8 b
-  showsPrec prec (Pow a b) = showParen (prec >= 9) $ showsPrec 9 a . showString " ^ " . showsPrec 9 b
+  showsPrec prec (Add a (Neg b)) = showParen (prec > 7) $ showsPrec 7 a . showString " - " . showsPrec 7 b
+  showsPrec prec (Add a b) = showParen (prec > 7) $ showsPrec 7 a . showString " + " . showsPrec 7 b
+  showsPrec prec (Mul a b) = showParen (prec > 8) $ showsPrec 8 a . showString " * " . showsPrec 8 b
+  showsPrec prec (Div a b) = showParen (prec > 8) $ showsPrec 8 a . showString " / " . showsPrec 8 b
+  showsPrec prec (Pow a b) = showParen (prec > 9) $ showsPrec 9 a . showString " ^ " . showsPrec 9 b
   showsPrec prec (Sin a) = showParen False $ showString "sin(" . shows a . showString ")"
   showsPrec prec (Cos a) = showParen False $ showString "cos(" . shows a . showString ")"
   showsPrec prec (Tan a) = showParen False $ showString "tan(" . shows a . showString ")"
@@ -65,6 +99,81 @@ diff c (Tan a) = Div (diff c a) (Pow (Cos a) (Const 2))
 diff c (Exp a) = Mul (diff c a) (Exp a)
 -- log(f(x)) -> f'(x) / f(x)
 diff c (Log a) = Div (diff c a) a
+
+--
+-- PARSING
+--
+
+-- Helper operators
+(<++>) a b = (++) <$> a <*> b
+
+(<:>) a b = (:) <$> a <*> b
+
+-- Parsers
+
+parseConst :: Parser (Expr Double)
+parseConst = do
+  neg <- option id (char '-' $> Neg)
+  value <- read <$> integer <++> decimal
+  return (neg (Const value))
+  where
+    integer = many1 digit
+    decimal = option "" $ char '.' <:> integer
+
+parseTerm :: Parser (Expr Double)
+parseTerm = parseParentheses <|> parseVar <|> parseConst
+
+parseParentheses :: Parser (Expr Double)
+parseParentheses = between (char '(') (char ')') parseFullExpr
+
+parseVar :: Parser (Expr Double)
+parseVar = Var <$> oneOf "xyz"
+
+parseFunc :: Parser (Expr Double)
+parseFunc =
+  choice
+    [ Sin <$> (string "sin" *> parseParentheses),
+      Cos <$> (string "cos" *> parseParentheses),
+      Tan <$> (string "tg" *> parseParentheses),
+      Exp <$> (string "exp" *> parseParentheses),
+      Log <$> (string "ln" *> parseParentheses)
+    ]
+
+parsePow :: Parser (Expr Double)
+parsePow = do
+  base <- parseTerm
+  _ <- char '^'
+  Pow base <$> parseTerm
+
+parseNeg :: Parser (Expr Double)
+parseNeg = Neg <$> (char '-' *> parseTerm)
+
+parseAddSub :: Parser (Expr Double -> Expr Double -> Expr Double)
+parseAddSub =
+  choice
+    [ char '+' $> Add,
+      char '-' $> (\x y -> Add x (Neg y))
+    ]
+
+parseMulDiv :: Parser (Expr Double -> Expr Double -> Expr Double)
+parseMulDiv =
+  choice
+    [ char '*' $> Mul,
+      char '/' $> Div
+    ]
+
+parseExpr :: Parser (Expr Double)
+parseExpr = try parsePow <|> try parseFunc <|> parseTerm
+
+parseFullExpr :: Parser (Expr Double)
+parseFullExpr = chainl1 parseExpr (parseAddSub <|> parseMulDiv)
+
+parseExpression :: String -> Either ParseError (Expr Double)
+parseExpression = parse parseFullExpr ""
+
+--
+-- SIMPLIFICATIONS
+--
 
 simplify :: (Num a, Eq a, Floating a) => Expr a -> Expr a
 simplify (Neg (Neg a)) = a
@@ -136,7 +245,7 @@ fullSimplify e = helper e (Const 0)
         next = simplify curr
 
 --
--- KALKULACJA WARTOSCI
+-- VALUE CALCULATION
 --
 
 myf :: (Floating a) => Char -> a
@@ -159,7 +268,7 @@ calc f (Exp a) = exp $ calc f a
 calc f (Log a) = log $ calc f a
 
 --
--- PRZYKLADY
+-- EXAMPLES
 --
 
 -- sin (1/x) * exp (y ^ 3 - 2)
@@ -190,69 +299,75 @@ am21 :: Expr Float
 am21 = Mul (Mul (Pow (Var 'x') (Const 3)) (Pow (Var 'y') (Const 2))) (Var 'z')
 
 --
--- TESTY
+-- TESTS
 --
 
-main :: IO ()
-main = do
-  let variable = 'x'
+-- main :: IO ()
+-- main = do
+--   let variable = 'x'
 
-  -- Test case 1: Derivative of a simple expression (x^2 + sin(x))
-  let expr1 = Add (Pow (Var 'x') (Const 2)) (Sin (Var 'x'))
-  putStrLn $ "Test Case 1 - Original expression: " ++ show expr1
-  putStrLn $ "Derivative with respect to '" ++ [variable] ++ "': " ++ show (dx expr1)
+--   -- Test case 1: Derivative of a simple expression (x^2 + sin(x))
+--   let expr1 = Add (Pow (Var 'x') (Const 2)) (Sin (Var 'x'))
+--   putStrLn $ "Test Case 1 - Original expression: " ++ show expr1
+--   putStrLn $ "Derivative with respect to '" ++ [variable] ++ "': " ++ show (dx expr1)
 
-  -- Test case 2: Derivative involving multiplication and division (x*sin(x))
-  let expr2 = Mul (Var 'x') (Sin (Var 'x'))
-  putStrLn $ "\nTest Case 2 - Original expression: " ++ show (expr2 :: Expr Double)
-  putStrLn $ "Derivative with respect to '" ++ [variable] ++ "': " ++ show (dx expr2)
+--   -- Test case 2: Derivative involving multiplication and division (x*sin(x))
+--   let expr2 = Mul (Var 'x') (Sin (Var 'x'))
+--   putStrLn $ "\nTest Case 2 - Original expression: " ++ show (expr2 :: Expr Double)
+--   putStrLn $ "Derivative with respect to '" ++ [variable] ++ "': " ++ show (dx expr2)
 
-  -- Test case 3: Derivative involving exponentiation (e^(x^2))
-  let expr3 = Exp (Pow (Var 'x') (Const 2))
-  putStrLn $ "\nTest Case 3 - Original expression: " ++ show expr3
-  putStrLn $ "Derivative with respect to '" ++ [variable] ++ "': " ++ show (dx expr3)
+--   -- Test case 3: Derivative involving exponentiation (e^(x^2))
+--   let expr3 = Exp (Pow (Var 'x') (Const 2))
+--   putStrLn $ "\nTest Case 3 - Original expression: " ++ show expr3
+--   putStrLn $ "Derivative with respect to '" ++ [variable] ++ "': " ++ show (dx expr3)
 
-  -- Test case 4: Derivative involving logarithm (ln(x))
-  let expr4 = Log (Var 'x')
-  putStrLn $ "\nTest Case 4 - Original expression: " ++ show (expr4 :: Expr Double)
-  putStrLn $ "Derivative with respect to '" ++ [variable] ++ "': " ++ show (dx expr4)
+--   -- Test case 4: Derivative involving logarithm (ln(x))
+--   let expr4 = Log (Var 'x')
+--   putStrLn $ "\nTest Case 4 - Original expression: " ++ show (expr4 :: Expr Double)
+--   putStrLn $ "Derivative with respect to '" ++ [variable] ++ "': " ++ show (dx expr4)
 
-  -- Test case 5: Derivative involving multiple operations (tan(x) + e^(x^2))
-  let expr5 = Add (Tan (Var 'x')) (Exp (Pow (Var 'x') (Const 2)))
-  putStrLn $ "\nTest Case 5 - Original expression: " ++ show expr5
-  putStrLn $ "Derivative with respect to '" ++ [variable] ++ "': " ++ show (dx expr5)
+--   -- Test case 5: Derivative involving multiple operations (tan(x) + e^(x^2))
+--   let expr5 = Add (Tan (Var 'x')) (Exp (Pow (Var 'x') (Const 2)))
+--   putStrLn $ "\nTest Case 5 - Original expression: " ++ show expr5
+--   putStrLn $ "Derivative with respect to '" ++ [variable] ++ "': " ++ show (dx expr5)
 
-  -- Test case 6: Derivative involving nested functions (sin(cos(x)))
-  let expr6 = Sin (Cos (Var 'x'))
-  putStrLn $ "\nTest Case 6 - Original expression: " ++ show (expr6 :: Expr Double)
-  putStrLn $ "Derivative with respect to '" ++ [variable] ++ "': " ++ show (dx expr6)
+--   -- Test case 6: Derivative involving nested functions (sin(cos(x)))
+--   let expr6 = Sin (Cos (Var 'x'))
+--   putStrLn $ "\nTest Case 6 - Original expression: " ++ show (expr6 :: Expr Double)
+--   putStrLn $ "Derivative with respect to '" ++ [variable] ++ "': " ++ show (dx expr6)
 
-  -- Test case 7: Derivative involving nested exponentiation (e^(x^(x^2)))
-  let expr7 = Exp (Pow (Var 'x') (Pow (Var 'x') (Const 2)))
-  putStrLn $ "\nTest Case 7 - Original expression: " ++ show expr7
-  putStrLn $ "Derivative with respect to '" ++ [variable] ++ "': " ++ show (dx expr7)
+--   -- Test case 7: Derivative involving nested exponentiation (e^(x^(x^2)))
+--   let expr7 = Exp (Pow (Var 'x') (Pow (Var 'x') (Const 2)))
+--   putStrLn $ "\nTest Case 7 - Original expression: " ++ show expr7
+--   putStrLn $ "Derivative with respect to '" ++ [variable] ++ "': " ++ show (dx expr7)
 
-  -- Test case 8: Derivative involving a fraction with complex numerator and denominator
-  let expr8 = Div (Add (Mul (Var 'x') (Sin (Var 'x'))) (Exp (Var 'x'))) (Var 'x')
-  putStrLn $ "\nTest Case 8 - Original expression: " ++ show (expr8 :: Expr Double)
-  putStrLn $ "Derivative with respect to '" ++ [variable] ++ "': " ++ show (dx expr8)
+--   -- Test case 8: Derivative involving a fraction with complex numerator and denominator
+--   let expr8 = Div (Add (Mul (Var 'x') (Sin (Var 'x'))) (Exp (Var 'x'))) (Var 'x')
+--   putStrLn $ "\nTest Case 8 - Original expression: " ++ show (expr8 :: Expr Double)
+--   putStrLn $ "Derivative with respect to '" ++ [variable] ++ "': " ++ show (dx expr8)
 
-  -- Test case 9: Derivative involving a combination of functions (tan(x^2) * ln(cos(x)))
-  let expr9 = Mul (Tan (Pow (Var 'x') (Const 2))) (Log (Cos (Var 'x')))
-  putStrLn $ "\nTest Case 9 - Original expression: " ++ show expr9
-  putStrLn $ "Derivative with respect to '" ++ [variable] ++ "': " ++ show (dx expr9)
+--   -- Test case 9: Derivative involving a combination of functions (tan(x^2) * ln(cos(x)))
+--   let expr9 = Mul (Tan (Pow (Var 'x') (Const 2))) (Log (Cos (Var 'x')))
+--   putStrLn $ "\nTest Case 9 - Original expression: " ++ show expr9
+--   putStrLn $ "Derivative with respect to '" ++ [variable] ++ "': " ++ show (dx expr9)
 
-  -- Test case 10: Derivative involving a combination of functions and constants (2 * x * e^(x^2))
-  let expr10 = Mul (Const 2) (Mul (Var 'x') (Exp (Pow (Var 'x') (Const 2))))
-  putStrLn $ "\nTest Case 10 - Original expression: " ++ show expr10
-  putStrLn $ "Derivative with respect to '" ++ [variable] ++ "': " ++ show (dx expr10)
+--   -- Test case 10: Derivative involving a combination of functions and constants (2 * x * e^(x^2))
+--   let expr10 = Mul (Const 2) (Mul (Var 'x') (Exp (Pow (Var 'x') (Const 2))))
+--   putStrLn $ "\nTest Case 10 - Original expression: " ++ show expr10
+--   putStrLn $ "Derivative with respect to '" ++ [variable] ++ "': " ++ show (dx expr10)
 
-  -- Test case 11: Derivative involving a combination of trigonometric and exponential functions
-  let expr11 = Mul (Sin (Var 'x')) (Exp (Mul (Var 'x') (Cos (Var 'x'))))
-  putStrLn $ "\nTest Case 11 - Original expression: " ++ show (expr11 :: Expr Double)
-  putStrLn $ "Derivative with respect to '" ++ [variable] ++ "': " ++ show (dx expr11)
+--   -- Test case 11: Derivative involving a combination of trigonometric and exponential functions
+--   let expr11 = Mul (Sin (Var 'x')) (Exp (Mul (Var 'x') (Cos (Var 'x'))))
+--   putStrLn $ "\nTest Case 11 - Original expression: " ++ show (expr11 :: Expr Double)
+--   putStrLn $ "Derivative with respect to '" ++ [variable] ++ "': " ++ show (dx expr11)
 
-  -- Test case 12: Derivative involving a more intricate combination of functions and constants
-  let expr12 = Div (Exp (Mul (Var 'x') (Var 'x'))) (Add (Sin (Var 'x')) (Mul (Const 2) (Cos (Var 'x'))))
-  putStrLn $ "\nTest Case 12 - Original expression: " ++ show expr12
-  putStrLn $ "Derivative with respect to '" ++ [variable] ++ "': " ++ show (dx expr12)
+--   -- Test case 12: Derivative involving a more intricate combination of functions and constants
+--   let expr12 = Div (Exp (Mul (Var 'x') (Var 'x'))) (Add (Sin (Var 'x')) (Mul (Const 2) (Cos (Var 'x'))))
+--   putStrLn $ "\nTest Case 12 - Original expression: " ++ show expr12
+--   putStrLn $ "Derivative with respect to '" ++ [variable] ++ "': " ++ show (dx expr12)
+
+--   let expr13 = Mul (Sin (Var 'x')) (Exp (Mul (Var 'y') (Cos (Var 'z'))))
+--   putStrLn $ "\nTest Case 13 - Original expression: " ++ show (expr13 :: Expr Double)
+--   putStrLn $ "Derivative with respect to 'x': " ++ show (dx expr13)
+--   putStrLn $ "Derivative with respect to 'y': " ++ show (dy expr13)
+--   putStrLn $ "Derivative with respect to 'z': " ++ show (dz expr13)
